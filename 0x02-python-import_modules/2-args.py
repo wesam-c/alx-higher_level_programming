5b9415f8612743d484f0595f188171ce240fbfb6
wesam-c <wesameldeeb15@gmail.com>
2024-01-05 02:03:10 +0400

    v

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     x = len(argv) - 1
     if x < 1:
         print("{} arguments.".format(x))
-    elif x  == 1:
+    elif x == 1:
         print("{} argument:".format(x))
     else:
         print("{} arguments:".format(x))
