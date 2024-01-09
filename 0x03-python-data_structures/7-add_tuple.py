75eb8755e4eb22f45109ba5959d052f0ad2dddb3
wesam-c <wesameldeeb15@gmail.com>
2024-01-09 18:45:40 +0400

    vvv

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1, a2 = tuple_a[:2] if len(tuple_a) >= 2 else (0, 0)
+    b1, b2 = tuple_b[:2] if len(tuple_b) >= 2 else (0, 0)
+
+    result_tuple = (a1 + b1, a2 + b2)
+
+    return result_tuple
