962b8794f00981bf3ee0371616efc6bc1e118cd4
wesam-c <wesameldeeb15@gmail.com>
2024-01-24 20:37:35 +0400

    classes0,1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""square module"""
+
+class square:
+    """defines square"""
+    pass #The class doesn't have any attributes - pass statement is a placeholder for future code
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class square:
+    """class definition"""
+
+    def __init__(self, size):
+        """constructor
+
+        args:
+            size: of the square
+        """
+        self.__size = size # The double underscore before size indicates it is a private variable.
