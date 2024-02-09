4f974e37abd29e1d0ed22dc7abe43b4c846c8ef2
wesam-c <wesameldeeb15@gmail.com>
2024-02-09 20:07:11 +0400

    python oop

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,24 @@ class Rectangle(Base):
             '''Returns string info about this rectangle.'''
             return '[{}] ({}) {}/{} - {}/{}'.\
                 format(type(self).__name, self.id, self.x, self.y, self.width, self.height)
+
+        def __update(self, id=None, width=None, height=None, x=None, y=None):
+            '''Internal method that updates instance attributes via */**args.'''
+            if id is not None:
+                self.id = id
+            if width is not None:
+                self.width = width
+            if height is not None:
+                self.height = height
+            if x is not None:
+                self.x = x
+            if y is not None:
+                self.y = y
+
+        def update(self, *args, **kwargs):
+            '''Update instance attributes via no_keyword & keyword args.'''
+            # print(args, kwargs)
+            if args:
+                self.__update(*args)
+            elif kwargs:
+                self.__update(**kwargs)
