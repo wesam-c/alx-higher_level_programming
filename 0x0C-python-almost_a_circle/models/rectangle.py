06371dbde96acf865f3583a457d4bc780a9f8897
wesam-c <wesameldeeb15@gmail.com>
2024-02-09 19:46:00 +0400

    python oop

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,3 +62,18 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
+        
+        def area(self):
+            '''Computes area of this rectangle.'''
+            return self.width * self.height
+
+        def display(self):
+            '''Prints string representation of this rectangle.'''
+            s = '\n' * self.y + \
+                (' ' * self.x + '#' * self.width + '\n') * self.height
+            print(s, end='')
+
+        def __str__(self):
+            '''Returns string info about this rectangle.'''
+            return '[{}] ({}) {}/{} - {}/{}'.\
+                format(type(self).__name, self.id, self.x, self.y, self.width, self.height)
