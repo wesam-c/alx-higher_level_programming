5286c4e137dfe878583cf606318a3c4c42dfe2ba
wesam-c <wesameldeeb15@gmail.com>
2024-03-02 14:59:20 +0400

    I&O

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""definig to_json_string function"""
+
+import  json
+
+
+def to_json_string(my_obj):
+    """returns json representation of an object"""
+    return json.dumps(my_obj)
+
