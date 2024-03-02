9c7ace20018a56624d34306135d2cc3b53746062
wesam-c <wesameldeeb15@gmail.com>
2024-03-02 14:47:32 +0400

    I&O

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """reads filename with utf-8"""
-    with open(filename, "w", encoding="utf-8" as file:
+    with open(filename, 'w', encoding="utf-8" as file:
             return file.write(text)
