8b4be8364fcf5b45e64d8006bb7edf1338c48a00
wesam-c <wesameldeeb15@gmail.com>
2024-03-13 08:58:46 +0400

    ADD

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+--  lists all records of 'second_table' of the database hbtn_0c_0
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Displays the num of records with id = 89
+-- in 'first_table' of database hbtn_0c_0 in your MySQL server.
+SELECT COUNT (*) FROM first_table WHERE id=89;
