bb95c4e6da720d422b0dd292b2c656c6e299b374
wesam-c <wesameldeeb15@gmail.com>
2024-04-27 04:03:03 +0400

    add task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+import sys
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data_encoded = urllib.parse.urlencode({'email': email})
+    data_encoded = data_encoded.encode('ascii')
+    request = urllib.request.Request(url, data_encoded)
+    with urllib.request.urlopen(request) as response:
+         print(response.read().decode())
+
