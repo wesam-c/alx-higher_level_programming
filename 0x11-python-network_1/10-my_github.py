0f0df2f895ea1cc615ca72ab6a9ca5c9558e78ae
wesam-c <wesameldeeb15@gmail.com>
2024-04-27 04:14:40 +0400

    add task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    headers = {"Accept": "application/vnd.github.v3+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "Authorization": f"Bearer {password}"}
+    response = requests.get(url, headers=headers)
+    print(response.json().get("id"))
